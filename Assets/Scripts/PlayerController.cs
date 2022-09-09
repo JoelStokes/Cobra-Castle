6d042de0059971605dfa2a4ed88fc869abfcda95
Joel Stokes <joel.stokes.e@gmail.com>
2022-09-08 23:10:28 -0400

    Full Snake Player Controller Art

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject BodyPrefab;
-    public Vector2 SpawnLocation;
+    public GameObject BodyPrefab;   //Pieces to instantiate on room start
+    public Vector2 SpawnLocation;   //Where player starts in room
 
-    public Sprite tailSprite;
-    public Sprite bodySprite;
+    //All sprites used in changing snake body
+    public Sprite tailDefaultImg;
+    public Sprite tailCurveRightImg;
+    public Sprite tailCurveLeftImg;
+    public Sprite bodyDefaultImg;
+    public Sprite bodyUpRightImg;   //90* flip is Right Down
+    public Sprite bodyUpLeftImg;
+    public Sprite bodyDownRightImg;
+    public Sprite bodyDownLeftImg;
+
 
     private int lives = 3;
     private int bodyLength = 4;
     private List<GameObject> Body = new List<GameObject>(); //All connected body parts
+    private List<SpriteRenderer> BodyRenderers = new List<SpriteRenderer>();    //All body part SpriteRenderers to change sprites based on move directions
     private List<Vector2> previousMoves = new List<Vector2>();  //List of previous moves to apply to body parts
 
     private float moveLimit;    //Modifier for how quickly the countdown is to the next move
@@ -36,11 +45,12 @@ public class PlayerController : MonoBehaviour
     {
         for (int i=0; i<bodyLength; i++){   //Create initial body
             GameObject BodySegment = GameObject.Instantiate(BodyPrefab, transform.position, Quaternion.identity);
+            BodyRenderers.Add(BodySegment.GetComponent<SpriteRenderer>());
 
             if (i < bodyLength - 1){    //Reset sprites on body parts
-                BodySegment.GetComponent<SpriteRenderer>().sprite = bodySprite;
+                BodyRenderers[BodyRenderers.Count-1].sprite = bodyDefaultImg;
             } else {
-                BodySegment.GetComponent<SpriteRenderer>().sprite = tailSprite;
+                BodyRenderers[BodyRenderers.Count-1].sprite = tailDefaultImg;
             }
 
             Body.Add(BodySegment);
@@ -96,14 +106,19 @@ public class PlayerController : MonoBehaviour
 
     private void PerformMove(Vector2 newMove){   //Move player, rotate body parts to match direction, remove oldest previousMove
         transform.position = new Vector3(transform.position.x + newMove.x, transform.position.y + newMove.y, transform.position.z);
-        transform.eulerAngles = new Vector3(0, 0, GetDirection(newMove, Vector2.zero, true));
+        transform.eulerAngles = new Vector3(0, 0, GetHeadDirection(newMove));
 
         for (int i=0; i<bodyLength; i++){
             Body[i].transform.position = new Vector3(previousMoves[i].x, previousMoves[i].y, transform.position.z);
-            if (i == 0){
-                Body[i].transform.eulerAngles = new Vector3(0, 0, GetDirection(new Vector2(transform.position.x, transform.position.y), previousMoves[i], false));
-            } else {
-                Body[i].transform.eulerAngles = new Vector3(0, 0, GetDirection(previousMoves[i-1], previousMoves[i], false));
+            if (i == 0){    //Body next to head (no nextPos)
+                Body[i].transform.eulerAngles = new Vector3(0, 0, 
+                    HandleBodyDirection(previousMoves[i], previousMoves[i+1], new Vector2(transform.position.x, transform.position.y), BodyRenderers[i]));
+            } else if (i < bodyLength-1){    //General Body
+                Body[i].transform.eulerAngles = new Vector3(0, 0, 
+                    HandleBodyDirection(previousMoves[i], previousMoves[i+1], previousMoves[i-1], BodyRenderers[i]));
+            } else {    //Tail
+                Body[i].transform.eulerAngles = new Vector3(0, 0, 
+                    GetTailDirection(previousMoves[i], previousMoves[i+1], previousMoves[i-1], BodyRenderers[i]));
             }
         }
             
@@ -111,24 +126,100 @@ public class PlayerController : MonoBehaviour
         previousMoves.RemoveAt(previousMoves.Count - 1);
     }
 
-    private float GetDirection(Vector2 newMove, Vector2 oldMove, bool head){   //Calculate direction moving for body rotation
-        Vector2 diff;
-        if (!head){
-            diff = new Vector2(Mathf.Abs(newMove.x) - Mathf.Abs(oldMove.x), Mathf.Abs(newMove.y) - Mathf.Abs(oldMove.y));
-        } else {
-            diff = newMove;
-        }
-
-        Debug.Log(newMove + "/ " + oldMove + "/ " + diff);
-
-        if (diff.x > 0){
+    private float GetHeadDirection(Vector2 newMove){    //Return current rotation of player's head
+        if (newMove.x > 0){
             return (float)Direction.Right;
-        } else if (diff.x < 0){
+        } else if (newMove.x < 0){
             return (float)Direction.Left;
-        } else if (diff.y > 0){
+        } else if (newMove.y > 0){
             return (float)Direction.Up;
         } else {
             return (float)Direction.Down;
+        }
+    }
+
+    private float HandleBodyDirection(Vector2 currentMove, Vector2 oldMove, Vector2 nextMove, SpriteRenderer currentRenderer){ //Return rotation of body part & update sprite
+        if (currentMove.x > oldMove.x && nextMove.x > currentMove.x){   //Straight Right
+            currentRenderer.sprite = bodyDefaultImg;
+            return (float)Direction.Right;
+        } else if (currentMove.x > oldMove.x && nextMove.y > currentMove.y) {  //Right Up
+            currentRenderer.sprite = bodyDownRightImg;            
+            return (float)Direction.Left;
+        } else if (currentMove.x > oldMove.x && nextMove.y < currentMove.y) {  //Right Down
+            currentRenderer.sprite = bodyUpRightImg;
+            return (float)Direction.Right;
+        } else if (currentMove.x < oldMove.x && nextMove.x < currentMove.x) {  //Straight Left
+            currentRenderer.sprite = bodyDefaultImg;
+            return (float)Direction.Left;
+        } else if (currentMove.x < oldMove.x && nextMove.y > currentMove.y) {  //Left Up
+            currentRenderer.sprite = bodyDownLeftImg;
+            return (float)Direction.Right;
+        } else if (currentMove.x < oldMove.x && nextMove.y < currentMove.y) {  //Left Down
+            currentRenderer.sprite = bodyUpLeftImg;
+            return (float)Direction.Left;
+        } else if (currentMove.y > oldMove.y && nextMove.y > currentMove.y) {  //Straight Up
+            currentRenderer.sprite = bodyDefaultImg;
+            return (float)Direction.Up;
+        } else if (currentMove.y < oldMove.y && nextMove.y < currentMove.y) {  //Straight Down
+            currentRenderer.sprite = bodyDefaultImg;
+            return (float)Direction.Down;
+        } else if (currentMove.y > oldMove.y && nextMove.x > currentMove.x) {  //Up Right
+            currentRenderer.sprite = bodyUpRightImg;
+            return (float)Direction.Up;
+        } else if (currentMove.y < oldMove.y && nextMove.x > currentMove.x) {  //Down Right
+            currentRenderer.sprite = bodyDownRightImg;
+            return (float)Direction.Up;
+        } else if (currentMove.y > oldMove.y && nextMove.x < currentMove.x) {  //Up Left
+            currentRenderer.sprite = bodyUpLeftImg;
+            return (float)Direction.Up;
+        } else if (currentMove.y < oldMove.y && nextMove.x < currentMove.x) {  //Down Left
+            currentRenderer.sprite = bodyDownLeftImg;
+            return (float)Direction.Up;
+        } else {
+            Debug.Log("Error! Unusual Direction Returned: " + currentMove + "/ " + oldMove + "/ " + nextMove);
+            return (float)Direction.Down;
+        }
+    }
+
+    private float GetTailDirection (Vector2 currentMove, Vector2 oldMove, Vector2 nextMove, SpriteRenderer currentRenderer){ //Return rotation of tail & update sprite
+         if (currentMove.x > oldMove.x && nextMove.x > currentMove.x){   //Straight Right
+            currentRenderer.sprite = tailDefaultImg;
+            return (float)Direction.Right;
+        } else if (currentMove.x > oldMove.x && nextMove.y > currentMove.y) {  //Right Up
+            currentRenderer.sprite = tailCurveLeftImg;
+            return (float)Direction.Right;
+        } else if (currentMove.x > oldMove.x && nextMove.y < currentMove.y) {  //Right Down
+            currentRenderer.sprite = tailCurveRightImg;
+            return (float)Direction.Right;
+        } else if (currentMove.x < oldMove.x && nextMove.x < currentMove.x) {  //Straight Left
+            currentRenderer.sprite = tailDefaultImg;
+            return (float)Direction.Left;
+        } else if (currentMove.x < oldMove.x && nextMove.y > currentMove.y) {  //Left Up
+            currentRenderer.sprite = tailCurveRightImg;
+            return (float)Direction.Left;
+        } else if (currentMove.x < oldMove.x && nextMove.y < currentMove.y) {  //Left Down
+            currentRenderer.sprite = tailCurveLeftImg;
+            return (float)Direction.Left;
+        } else if (currentMove.y > oldMove.y && nextMove.y > currentMove.y) {  //Straight Up
+            currentRenderer.sprite = tailDefaultImg;
+            return (float)Direction.Up;
+        } else if (currentMove.y < oldMove.y && nextMove.y < currentMove.y) {  //Straight Down
+            currentRenderer.sprite = tailDefaultImg;
+            return (float)Direction.Down;
+        } else if (currentMove.y > oldMove.y && nextMove.x > currentMove.x) {  //Up Right
+            currentRenderer.sprite = tailCurveRightImg;
+            return (float)Direction.Up;
+        } else if (currentMove.y < oldMove.y && nextMove.x > currentMove.x) {  //Down Right
+            currentRenderer.sprite = tailCurveLeftImg;
+            return (float)Direction.Down;
+        } else if (currentMove.y > oldMove.y && nextMove.x < currentMove.x) {  //Up Left
+            currentRenderer.sprite = tailCurveLeftImg;
+            return (float)Direction.Up;
+        } else if (currentMove.y < oldMove.y && nextMove.x < currentMove.x) {  //Down Left
+            currentRenderer.sprite = tailCurveRightImg;
+            return (float)Direction.Down;
+        } else {    //Mouse eaten, currentMove & oldMove the same
+            return Body[Body.Count-1].transform.eulerAngles.z;
         }
     }
 
@@ -137,11 +228,13 @@ public class PlayerController : MonoBehaviour
 
         GameObject BodySegment = GameObject.Instantiate(BodyPrefab, new Vector3(
             previousMoves[previousMoves.Count - 1].x, previousMoves[previousMoves.Count - 1].y, transform.position.z), Quaternion.identity);
-        BodySegment.GetComponent<SpriteRenderer>().sprite = tailSprite;
-        Body[Body.Count-1].GetComponent<SpriteRenderer>().sprite = bodySprite;
-
+            
         Body.Add(BodySegment);
+        BodyRenderers.Add(BodySegment.GetComponent<SpriteRenderer>());
         previousMoves.Add(previousMoves[previousMoves.Count - 1]);
+
+        BodyRenderers[BodyRenderers.Count-1].sprite = BodyRenderers[BodyRenderers.Count-2].sprite;
+        Body[Body.Count-1].transform.rotation = Body[Body.Count-2].transform.rotation;
     }
 
     private void Die(){
